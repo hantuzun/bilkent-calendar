749d887fee810ddc5889f075bd57eeb8ac7b874b
emrehan <emrehan.tuzun@gmail.com>
2015-02-03 14:57:36 +0200

    Refactor

--- crawler.py
+++ crawler.py
@@ -4,23 +4,34 @@ import html
 import json
 import re
 
-response = requests.get('http://www.bilkent.edu.tr/bilkent/academic/calendar/')
-print('status: ' + str(response.status_code))
-soup = BeautifulSoup(response.text)
-tables = soup.findAll('table', {'class': 'sample'})
 
-events = []
-for table in tables:
-	for row in table.findAll('tr'):
-		aux = row.findAll('td')
-		date = re.sub(r', [A-Z][a-z]+day', r'', aux[0].text)
-		summary = html.unescape(aux[1].string)
-		event = {}
-		event['date'] = ' '.join(date.split())
-		event['summary'] = ' '.join(summary.split())
-		event['idmyo'] = aux[1].find('span', {'class': 'idmyo'}) is not None
-		events.append(event)
+def main():
+	# get html data from the Bilkent website
+	url = 'http://www.bilkent.edu.tr/bilkent/academic/calendar/'
+	print('getting data from ' + url)
+	response = requests.get(url)
+	print('status: ' + str(response.status_code))
+	soup = BeautifulSoup(response.text)
+	tables = soup.findAll('table', {'class': 'sample'})
 
-file = open('events.json', 'w')
-file.write(json.dumps(events, indent = 4))
-file.close()
+	# create serializable events from the html data
+	events = []
+	for table in tables:
+		for row in table.findAll('tr'):
+			aux = row.findAll('td')
+			date = re.sub(r', [A-Z][a-z]+day', r'', aux[0].text)
+			summary = html.unescape(aux[1].string)
+			event = {}
+			event['date'] = ' '.join(date.split())
+			event['summary'] = ' '.join(summary.split())
+			event['idmyo'] = aux[1].find('span', {'class': 'idmyo'}) is not None
+			events.append(event)
+
+	# save events
+	file = open('events.json', 'w')
+	file.write(json.dumps(events, indent = 4))
+	file.close()
+
+
+if __name__ == "__main__":
+    main()
--- creator.py
+++ creator.py
@@ -8,57 +8,62 @@ import random
 import json
 import re
 
-calendar_name = 'Bilkent Academic Calendar'
-cal = Calendar()
-cal['summary'] = calendar_name
-cal['dtstart'] = '20050404T080000'
-cal.add('prodid', '-//Bilkent Academic Calendar//tuzun.co//')
-cal.add('version', '0.1')
 
-file = open('events.json','r')
-events = json.load(file)
-file.close()
+def main():
+	# create a calendar
+	calendar_name = 'Bilkent Academic Calendar'
+	cal = Calendar()
+	cal['summary'] = calendar_name
+	cal['dtstart'] = '20050404T080000'
+	cal.add('prodid', '-//' + calendar_name + '//tuzun.co//')
+	cal.add('version', '0.1')
 
-events_with_multiple_dates = filter(lambda x: (x['date'].find(',') != -1), events)
-for e in events_with_multiple_dates:
-	dates = e['date'].replace(',', ' ')
-	days = re.findall('\d{1,2}[ ]', dates)
-	tail = re.search('\w+ \d{4}', dates).group(0)
-	for day in days:
-		date = day + tail
-		partial_event = {'date': date, 'summary': e['summary'], 'idmyo': e['idmyo']}
-		events.append(partial_event)
+	# get events
+	file = open('events.json','r')
+	events = json.load(file)
+	file.close()
 
-events_plain = filter(lambda x: (not any((c in set('-(,')) for c in x['date'])), events)
-events_with_time = filter(lambda x: (x['date'].find('(') != -1), events)
-events_with_range = filter(lambda x: (x['date'].find('-') != -1), events)
+	# split events with multiple days in the same month
+	for e in events:
+		if (e['date'].find(',') != -1):
+			events.remove(e)
+			date = e['date'].replace(',', ' ')
+			days = re.findall('\d{1,2}[ ]', date)
+			tail = re.search('\w+ \d{4}', date).group(0)
+			for day in days:
+				date = day + tail
+				partial_event = {'date': date, 'summary': e['summary'], 'idmyo': e['idmyo']}
+				events.append(partial_event)
+			
+	# add events to the calendar
+	for e in events:
+		add_event(e, cal)
 
-for e in events_plain:
+	# save the calendar
+	f = open(os.path.join(expanduser('~'), calendar_name + '.ics'), 'wb')
+	print(calendar_name + '.ics has been created in ' + os.path.join(expanduser('~')))
+	f.write(cal.to_ical())
+	f.close()
+
+
+def add_event(e, cal):
 	event = Event()
 	event['uid'] = str(random.randrange(0, 100000000000000000000)) + '@tuzun.co'
 	event.add('summary', e['summary'])
-	event.add('dtstart', datetime.fromtimestamp(mktime(strptime(e['date'], '%d %B %Y'))))
+	date = e['date']
+	if ((date.find('(') == -1) and (date.find('-') == -1)):
+		# plain dates
+		event.add('dtstart', datetime.fromtimestamp(mktime(strptime(date, '%d %B %Y'))))
+	if (date.find('(') != -1):
+		# dates with specific time
+		event.add('dtstart', datetime.fromtimestamp(mktime(strptime(date, '%d %B %Y (%H:%M)'))))
+	if (date.find('-') != -1):
+		# dates with range
+		dtstart, dtend = date.split(' - ')
+		event.add('dtstart', datetime.fromtimestamp(mktime(strptime(dtstart, '%d %B %Y'))))
+		event.add('dtend', datetime.fromtimestamp(mktime(strptime(dtend, '%d %B %Y'))) + timedelta(hours=23, minutes=59))
 	cal.add_component(event)
 
-for e in events_with_time:
-	event = Event()
-	event['uid'] = str(random.randrange(0, 100000000000000000000)) + '@tuzun.co'
-	event.add('summary', e['summary'])
-	event.add('dtstart', datetime.fromtimestamp(mktime(strptime(e['date'], '%d %B %Y (%H:%M)'))))
-	cal.add_component(event)
 
-for e in events_with_range:
-	event = Event()
-	event['uid'] = str(random.randrange(0, 100000000000000000000)) + '@tuzun.co'
-	event.add('summary', e['summary'])
-	dates = e['date'].split(' - ')
-	dtstart = dates[0]
-	dtend = dates[1]
-	event.add('dtstart', datetime.fromtimestamp(mktime(strptime(dtstart, '%d %B %Y'))))
-	event.add('dtend', datetime.fromtimestamp(mktime(strptime(dtend, '%d %B %Y'))) + timedelta(hours=23, minutes=59))
-	cal.add_component(event)
-
-f = open(os.path.join(expanduser('~'), calendar_name + '.ics'), 'wb')
-print(calendar_name + '.ics has been created in ' + os.path.join(expanduser('~')))
-f.write(cal.to_ical())
-f.close()
+if __name__ == "__main__":
+    main()
